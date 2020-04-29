c824cc80f2bda0c6ed5742dda3808cc85efba910
KohHBT <52650667+KohHBT@users.noreply.github.com>
2020-04-28 22:11:41 -0500

    second report first step

---
+++ first step- distinguish 2 faces/face_reg.py
@@ -0,0 +1,16 @@
+import face_recognition
+
+picture_of_me = face_recognition.load_image_file("Andrew.jpg")
+my_face_encoding = face_recognition.face_encodings(picture_of_me)[0]
+
+
+unknown_picture = face_recognition.load_image_file("Thuy.jpg")
+unknown_face_encoding = face_recognition.face_encodings(unknown_picture)[0]
+
+
+results = face_recognition.compare_faces([my_face_encoding], unknown_face_encoding)
+
+if results[0] == True:
+    print("It's a picture of Andrew!")
+else:
+    print("It's not a picture of Andrew!")
